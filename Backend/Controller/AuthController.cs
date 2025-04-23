5f710773878c77f3c4033890c14c706ad8c54442
YaniAnis <155096499+YaniAnis@users.noreply.github.com>
2025-04-23 05:12:45 +0100

    Add files via upload

---
+++ Backend/Controller/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetV1.Data;
+using ProjetV1.Models;
+
+namespace ProjetV1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(Utilisateur utilisateur)
+        {
+
+            if (_context.Utilisateurs.Any(u => u.Email == utilisateur.Email))
+            {
+                return BadRequest("Cet email est déjà utilisé.");
+            }
+
+            _context.Utilisateurs.Add(utilisateur);
+            await _context.SaveChangesAsync();
+            return Ok(utilisateur);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto data)
+        {
+            var user = _context.Utilisateurs
+                .FirstOrDefault(u => u.Email == data.Email && u.MotDePasse == data.MotDePasse);
+
+            if (user == null)
+                return Unauthorized("Email ou mot de passe incorrect");
+
+            return Ok(new { message = "Connexion réussie", user });
+        }
+    }
+
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string MotDePasse { get; set; }
+    }
+}
---
+++ Backend/Controller/TicketController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetV1.Data;
+using ProjetV1.Models;
+
+namespace ProjetV1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TicketController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get() => Ok(_context.Tickets.ToList());
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Ticket ticket)
+        {
+            _context.Tickets.Add(ticket);
+            _context.SaveChanges();
+            return Ok(ticket);
+        }
+    }
+}
