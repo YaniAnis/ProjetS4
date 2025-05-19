2dde41bfb87f19d08fe71603a9ef26ffce543c19
nacimtali <155154468+nacimtali@users.noreply.github.com>
2025-05-19 12:18:03 +0100

    Add files via upload

---
+++ Frontend/mot-de-passe-oublié/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import PasswordReset from "./components/PasswordReset.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordReset />
+    </div>
+  )
+}
+
+export default App
