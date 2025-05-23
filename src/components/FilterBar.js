37c1ea3232f7a02dc6661b127f55ecbbe1e096c7
nacimtali <155154468+nacimtali@users.noreply.github.com>
2025-05-23 14:54:12 +0100

    main

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,4 +1,12 @@
 "use client"
+import Select from "react-select";
+
+const leagueOptions = [
+  { value: "", label: "Toutes les ligues" },
+  { value: "Ligue 1 Mobilis", label: "Ligue 1 Mobilis" },
+  { value: "Ligue 2 Mobilis Est", label: "Ligue 2 Mobilis Est" },
+  { value: "Ligue 2 Mobilis Ouest", label: "Ligue 2 Mobilis Ouest" },
+];
 
 function FilterBar({ searchTerm, setSearchTerm, leagueFilter, setLeagueFilter }) {
   return (
@@ -16,21 +24,41 @@ function FilterBar({ searchTerm, setSearchTerm, leagueFilter, setLeagueFilter })
         </button>
       </div>
       <div className="filters">
-        <select id="filter-league" value={leagueFilter} onChange={(e) => setLeagueFilter(e.target.value)}>
-          <option value="">Toutes les ligues</option>
-          <option value="Ligue 1">Ligue 1</option>
-          <option value="Champions League">Champions League</option>
-          <option value="Coupe Algérie">Coupe Algérie</option>
-        </select>
-        <select id="filter-month" hidden>
-          <option value="">Toutes les ligues</option>
-          <option value="ligue1">Ligue 1</option>
-          <option value="Champions League">Champions League</option>
-          <option value="Europa League">Coupe Algérie</option>
-        </select>
+        <Select
+          id="filter-league"
+          value={leagueOptions.find((opt) => opt.value === leagueFilter)}
+          onChange={(opt) => setLeagueFilter(opt.value)}
+          options={leagueOptions}
+          styles={{
+            option: (provided, state) => ({
+              ...provided,
+              backgroundColor: state.isSelected
+                ? "#1a472a"
+                : state.isFocused
+                ? "#f2f2f2"
+                : undefined,
+              color: state.isSelected ? "#fff" : undefined,
+              fontWeight: state.isSelected ? "bold" : undefined,
+            }),
+            control: (provided, state) => ({
+              ...provided,
+              backgroundColor: "#1a472a",
+              color: "#fff",
+              boxShadow: "none",         // retire la bordure bleue
+              borderColor: "#1a472a",    // couleur de la bordure (vert)
+              "&:hover": {
+                borderColor: "#1a472a",  // garde la bordure verte au hover
+              },
+            }),
+            singleValue: (provided) => ({
+              ...provided,
+              color: "#fff",
+            }),
+          }}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default FilterBar
+export default FilterBar;
